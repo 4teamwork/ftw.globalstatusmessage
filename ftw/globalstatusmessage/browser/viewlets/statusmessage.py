516c18aea591fe90b255b1b0933b431fabc339ec
Timon Tschanz <timon.tschanz@4teamwork.ch>
2010-08-20 15:06:29 +0200

    added missing files

---
+++ ftw/globalstatusmessage/browser/viewlets/statusmessage.py
@@ -0,0 +1,5 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets import common
+
+class StatusmessageViewlet(common.PathBarViewlet):
+    render = ViewPageTemplateFile('statusmessage.pt')
