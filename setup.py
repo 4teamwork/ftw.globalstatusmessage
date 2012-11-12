68c2cd94505f4efa239ac42affa9fb1b9fb1af4b
Jonas Baumann <jone@jone.ch>
2012-11-12 14:23:00 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ import os
 
 
 tests_require = [
-      'collective.testcaselayer',
-      'Plone']
+    'collective.testcaselayer',
+    'Plone',
+    ]
 
 
 def read(*rnames):
@@ -17,30 +18,35 @@ maintainer = 'Mathias Leimgruber'
 setup(name='ftw.globalstatusmessage',
       version=version,
       description="Shows a global message on every site.",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         ],
-      keywords='',
+
+      keywords='ftw global status message plone',
       author='4teamwork GmbH',
-      author_email='info@4teamwork.ch',
-      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw-globalstatusmessage',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.globalstatusmessage',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-          'setuptools',
-          'z3c.autoinclude',
-          'plone.z3cform',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'z3c.autoinclude',
+        'plone.z3cform',
+        # -*- Extra requirements: -*-
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
