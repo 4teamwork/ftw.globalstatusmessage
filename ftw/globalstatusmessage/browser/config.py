90b018abe86ef74b81ce9a70b705edeec9999c35
Daniel Jowett <d.jowett@4teamwork.ch>
2019-09-03 14:58:30 +0200

    Remove unused plone.app.form dependency

--- ftw/globalstatusmessage/browser/config.py
+++ ftw/globalstatusmessage/browser/config.py
@@ -1,6 +1,5 @@
 from ftw.globalstatusmessage import _
 from ftw.globalstatusmessage.interfaces import IStatusMessageConfigForm
-from plone.app.form.widgets.wysiwygwidget import WYSIWYGWidget
 from plone.z3cform.layout import wrap_form
 from z3c.form import form, field
 
@@ -9,7 +8,6 @@ class GlobalStatusMessageForm(form.EditForm):
     """This class defines the form"""
     schema = IStatusMessageConfigForm
     fields = field.Fields(IStatusMessageConfigForm)
-    fields['message_textfield'].custom_widget = WYSIWYGWidget
 
     label = _(u"Global Statusmessage")
 
